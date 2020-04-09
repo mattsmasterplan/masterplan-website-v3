31ea2964ae22449c0020a2b252d8300d38aef561
Matt B <bury45@gmail.com>
2020-04-09 02:24:07 -0400

    removed unused code

--- src/app/pages/getting-started-typescript/getting-started-typescript.component.ts
+++ src/app/pages/getting-started-typescript/getting-started-typescript.component.ts
@@ -10,35 +10,3 @@ export class GettingStartedTypescriptComponent implements OnInit {
 
   ngOnInit(): void {}
 }
-
-//let isDone: boolean = 'false';
-
-//isDone = true;
-
-let x: [string, number];
-x = ['hello', 10];
-
-console.log(x[0].substring(1));
-
-enum Color {
-  Red = 1,
-  Green,
-  Blue
-}
-
-let c: Color = Color.Green;
-let colorName: string = Color[2];
-
-function warnUser(): void {
-  console.log('This is my warning message');
-}
-
-let u: undefined = undefined;
-let n: null = null;
-
-function error(message: string): never {
-  throw new Error(message);
-}
-
-let input = [1, 2];
-let [first, second] = input;
