7b1f05dea3b492757bf5e894f111429111fd78bd
Matt B <bury45@gmail.com>
2020-04-14 01:22:17 -0400

    Added Enum information to TS page

--- src/app/pages/getting-started-typescript/getting-started-typescript.component.ts
+++ src/app/pages/getting-started-typescript/getting-started-typescript.component.ts
@@ -10,3 +10,10 @@ export class GettingStartedTypescriptComponent implements OnInit {
 
   ngOnInit(): void {}
 }
+
+enum Direction {
+    Up = "UP",
+    Down = "DOWN",
+    Left = "LEFT",
+    Right = "RIGHT",
+}
